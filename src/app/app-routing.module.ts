dab65d65e6be6d8d534f24a3cdac3c6fd4b792f6
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2024-10-13 13:25:23 +0700

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login/login.component';
+import { AdminpageComponent } from './components/admin/adminpage/adminpage.component';
+import { UserManagerComponent } from './components/admin/user-manager/user-manager.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: '',
+    component: AdminpageComponent,
+    children: [
+      {
+        path: 'homepage',
+        component: DashboardComponent
+      },
+      {
+        path: 'user-manager',
+        component:UserManagerComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/admin/navbar/navbar.component';
+import { HeaderComponent } from './components/admin/header/header.component';
+import { FooterComponent } from './components/admin/footer/footer.component';
+import { UserManagerComponent } from './components/admin/user-manager/user-manager.component';
+import { LoginComponent } from './components/login/login/login.component';
+import { AdminpageComponent } from './components/admin/adminpage/adminpage.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserManagerComponent,
+    LoginComponent,
+    AdminpageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
